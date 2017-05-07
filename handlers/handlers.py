3019c4e7d5951237c5c41662deea9bad26df11c1
Drew Miller <atm08e@gmail.com>
2017-05-07 01:07:48 -0400

    Refactoring handlers into a class

---
+++ handlers/handlers.py
@@ -0,0 +1,58 @@
+import logging
+import sys
+import pprint
+
+
+from aiohttp import web
+import aiohttp_jinja2
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+class Handlers():
+
+    @aiohttp_jinja2.template('index.html')
+    async def root(request):
+        context = {'name': 'Andrew', 'surname': 'Svetlov'}
+        response = aiohttp_jinja2.render_template('index.html',
+                                                  request,
+                                                  context)
+        response.headers['amiller.im-custom'] = 'en'
+        return response
+
+    @aiohttp_jinja2.template('about.html')
+    async def about(request):
+        return {'about': 'derp'}
+
+    @aiohttp_jinja2.template('links.html')
+    async def links(request):
+        return {'about': 'derp'}
+
+    @aiohttp_jinja2.template('blog.html')
+    async def blog(request):
+        return {'about': 'derp'}
+
+    @aiohttp_jinja2.template('login.html')
+    async def login(request):
+        return {'about': 'derp'}
+
+    @aiohttp_jinja2.template('register.html')
+    async def register(request):
+        return {'about': 'derp'}
+
+    @aiohttp_jinja2.template('snowboarding.html')
+    async def snowboarding(request):
+        year = request.match_info.get('year', None)
+        # TODO check year and default
+        gallery = request.app['galleries']['snowboarding'][year]
+        logger.debug(gallery)
+        return gallery
+
+    @aiohttp_jinja2.template('under_construction.html')
+    async def under_construction(request):
+        return {'about': 'derp'}
+
+    @aiohttp_jinja2.template('under_construction.html')
+    async def test(request):
+        return {'about': 'derp'}
