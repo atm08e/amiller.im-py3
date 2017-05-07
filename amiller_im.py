9289f710910990dd2846ca0d3f677a5ae546113d
Drew Miller <atm08e@gmail.com>
2017-05-07 01:07:11 -0400

    Loading gallery json at init time

--- amiller_im.py
+++ amiller_im.py
@@ -1,6 +1,7 @@
+import json
 import logging
 import sys
-import handlers
+from handlers.handlers import Handlers
 import os
 import routes
 #
@@ -22,11 +23,25 @@ class AmillerIm:
         app['path_to_templates'] = '{}/{}'.format(app['path_to_app_root'], 'templates')
         app['path_to_static'] = '{}/{}'.format(app['path_to_app_root'], 'static')
 
+        path_to_snowboarding_2016 = '{}/{}/{}/{}'.format(app['path_to_static'], 'files', 'snowboarding',
+                                             'snowboarding-2016.json')
+        logger.debug('Trying to open file: {}'.format(path_to_snowboarding_2016))
+        snowboarding_2016 = None
+        with open(path_to_snowboarding_2016, 'r+') as f:
+            # TODO async json
+            snowboarding_2016 = json.load(f)
+
+        app['galleries'] = {
+            'snowboarding': {'2016': snowboarding_2016}
+        }
+
+
+
         aiohttp_jinja2.setup(app,
                              context_processors=[aiohttp_jinja2.request_processor],
                              loader=jinja2.FileSystemLoader(app['path_to_templates']))
 
-        routes.setup(app=app, handler=handlers)
+        routes.setup(app=app, handler=Handlers)
         return app
 
 
