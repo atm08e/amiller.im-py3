b37eba65b639ccb227288ae63c9c512b4b57585f
andrew miller <atm08e@gmail.com>
2018-07-14 15:06:08 -0700

    Adding spruce call in get_secret.sh

--- ci/scripts/get_secrets.sh
+++ ci/scripts/get_secrets.sh
@@ -2,4 +2,8 @@
 
 echo "Attempting to get secrets from vault..."
 
+spruce merge \
+--prune meta \
+amiller_im_git/ci/config/secrets.yml > secrets/secrets.yml
+
 echo "Secrets file successfully generated"
