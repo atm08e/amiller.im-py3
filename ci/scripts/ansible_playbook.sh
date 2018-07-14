34e72d957931f6c2432ac3d5c98a4e3c9bc06dd7
andrew miller <atm08e@gmail.com>
2018-07-14 15:46:18 -0700

    Debug

--- ci/scripts/ansible_playbook.sh
+++ ci/scripts/ansible_playbook.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 uname -a
 which ansible
--- ci/scripts/get_secrets.sh
+++ ci/scripts/get_secrets.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-
+set -e
 echo "Attempting to get secrets from vault..."
 
-spruce merge \
---prune meta \
-amiller_im_git/ci/config/secrets.yml > secrets/secrets.yml
+# TODO
+# spruce merge \
+# --prune meta \
+# amiller_im_git/ci/config/secrets.yml > secrets/secrets.yml
 
 echo "Secrets file successfully generated"
